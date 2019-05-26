71a4e1d8b6fa047f3216be9b3ccd6aa907de9280
Stella McEwen <mcewen.stella@gmail.com>
2019-05-26 23:54:26 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,5 +1,10 @@
 class Course 
   
-  attr_accessor :title, :schedule, :description
+  attr_accessor :title, :schedule, :description 
+  
+  @@all = [] 
+  
+  def initialize 
+    @@all << self
 end
 
