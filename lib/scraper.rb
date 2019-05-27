c91d4a6f41e00564965cd37bb8384f125b19e71a
Stella McEwen <mcewen.stella@gmail.com>
2019-05-27 00:15:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,11 +18,14 @@ class Scraper
   end
    
   
-   def get_page
- 
-  Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    end
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
+    
+  def get_courses
+    self.get_page.css(".post")
+  end
+  
   
   
   
